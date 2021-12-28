f16e9eb897cc944adc24e3a79467b60bfb9a1e79
Nonoka77 <ptxdy77@gmail.com>
2021-12-28 10:25:26 +0900

    Add: TypeScriptの設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require("path");
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: "./src/index.tsx",
+	module: {
+		rules: [
+			{
+				test: /\.tsx$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".js", ".ts", ".tsx"],
+	},
 	output: {
-		path: path.resolve(_dirname, "dist"),
+		path: path.resolve(__dirname, "dist"),
 		filename: "index.js",
 		publicPath: "dist/",
 	},
